1a733b9c02e8dc204bf4417d45219911901a020e
Joe Hanink <jhanink@gmail.com>
2023-09-22 14:04:44 -0700

    update test name

--- tests/obs-scores.spec.ts
+++ tests/obs-scores.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('OBS stable url redirect to DP', async ({ page }) => {
   await page.goto('http://localhost/obs-scores.html?tourney=test&table=2');
 
   await expect(page.locator('.ant-layout')).toHaveText(/Race to/);
